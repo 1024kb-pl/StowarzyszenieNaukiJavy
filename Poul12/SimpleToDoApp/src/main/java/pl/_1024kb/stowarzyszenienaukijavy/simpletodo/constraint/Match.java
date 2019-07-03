c4a384c80d755e792a6e9c31e24e81078391a5ee
poul124 <poulg12@gmail.com>
2019-07-03 17:42:00 +0200

    fix footer

--- Poul12/SimpleToDoApp/src/main/java/pl/_1024kb/stowarzyszenienaukijavy/simpletodo/constraint/Match.java
+++ Poul12/SimpleToDoApp/src/main/java/pl/_1024kb/stowarzyszenienaukijavy/simpletodo/constraint/Match.java
@@ -3,8 +3,8 @@ package pl._1024kb.stowarzyszenienaukijavy.simpletodo.constraint;
 import pl._1024kb.stowarzyszenienaukijavy.simpletodo.service.MatchValidator;
 
 import javax.validation.Constraint;
-import javax.validation.Payload;
-import java.lang.annotation.*;
+        import javax.validation.Payload;
+        import java.lang.annotation.*;
 
 @Documented
 @Constraint(validatedBy = MatchValidator.class)
